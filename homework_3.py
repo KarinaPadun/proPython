77f5c8f5cf47ff6332bce445f4d27da21646fcd5
karina <kmalina946@gmail.com>
2024-02-04 00:19:49 +0200

    edditing

--- homework_3.py
+++ homework_3.py
@@ -1,29 +1,33 @@
 import random
 
+
 class Car:
-    def __init__(self, model , color):
+    def __init__(self, model, color):
         self.fuel = random.randrange(0, 9)
         self.trip_distance = 0
         self.model = model
         self.color = color
+
     def move(self, distance):
         if self.fuel >= distance:
-            self.fuel -=distance
+            self.fuel -= distance
+            self.trip_distance += distance
             return True
         else:
-            print(f"Недостатньо пального для проїзду {distance} км.")
+            print(f"{self.color} {self.model} не має достатньо пального для проїзду {distance} км.")
             return False
 
 
-car_1 = Car('Mercedes', 'black',  )
-car_2 = Car('Audi', 'pink')
-car_3 = Car('BMW', 'white')
+car_1 = Car('Mercedes', 'Black')
+car_2 = Car('Audi', 'Pink')
+car_3 = Car('BMW', 'White')
 
-desired_dist = 100
+desired_dist = 10
 race_dist = 0
+winner = None
 
 while race_dist < desired_dist:
-
+    fuel_empty = True
     for car in [car_1, car_2, car_3]:
         distance = random.randrange(0, 9)
         path = car.move(distance)
@@ -31,14 +35,20 @@ while race_dist < desired_dist:
             race_dist += distance
             print(f"{car.color} {car.model} проїхав {distance} км. Загальна відстань: {race_dist} км.")
             if race_dist >= desired_dist:
-                print(f"\n{car.color} {car.model} переміг! Відстань: {car.trip_distance} км.")
+                winner = car
                 break
 
-        #
+            if car.fuel > 0:
+                fuel_empty = False
+
+    if fuel_empty:
+        print("\nЗакінчилося пальво для всіх машин.")
+        break
+
 for car in [car_1, car_2, car_3]:
     print(f"\n{car.color} {car.model}:")
     print(f"  Пройдена відстань: {car.trip_distance} км.")
     print(f"  Запас палива: {car.fuel} одиниць.\n")
 
-
-
+if winner:
+    print(f"\n{winner.color} {winner.model} переміг! Відстань: {winner.trip_distance} км.")
