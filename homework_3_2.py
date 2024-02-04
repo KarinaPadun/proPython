0e2118ffed46895087075e84ce1401880469cf35
karina <kmalina946@gmail.com>
2024-02-04 20:18:31 +0200

    edditing

--- homework_3_2.py
+++ homework_3_2.py
@@ -1,49 +1,58 @@
-# черновой вариант
+# 2 вариант
 import random
+
 
 class Car:
     def __init__(self, model, color):
+        """
+        Функція є конструктором класу.Призначена для ініціалізації об'єкта класу з заданими параметрами.
+        :param model:
+        :param color:
+        """
         self.fuel = random.randrange(0, 9)
         self.trip_distance = 0
         self.model = model
         self.color = color
 
     def move(self, distance):
-        if self.fuel > 0:
-            # Визначаємо відстань, яку може проїхати автомобіль
-            distance_covered = min(self.fuel, distance)
-            # Оновлюємо властивості автомобіля
-            self.trip_distance += distance_covered
-            self.fuel -= distance_covered
-            return distance_covered
+        """
+        Функція призначена для моделювання переміщення автомобіля на визначену відстань
+        :param distance:
+        :return: Повертає True, щоб показати успішну подорож,Повертає False, щоб показати,
+        що подорож не відбулася через нестачу пального.
+        """
+        if self.fuel >= distance:
+            self.trip_distance += distance
+            self.fuel -= distance
+            return True
         else:
-            return 0
+            print(f"{self.model} не вистачає палива, щоб проїхати {distance} км.")
+            return False
 
-# Створення екземплярів класу
-car1 = Car(model="Toyota", color="Blue")
-car2 = Car(model="Honda", color="Red")
-car3 = Car(model="Ford", color="Green")
 
-# Налаштування гонки
-desired_dist = 50
+car1 = Car("Toyota", "red")
+car2 = Car("BMW", "blue")
+car3 = Car("Mercedes", "black")
+
+desired_dist = 100
 race_dist = 0
-
-# Цикл гонки
 while race_dist < desired_dist:
-    # Рух кожного автомобіля
-    dist1 = car1.move(random.randrange(0, 9))
-    dist2 = car2.move(random.randrange(0, 9))
-    dist3 = car3.move(random.randrange(0, 9))
+    fuel_empty = True
+    for car in [car1, car2, car3]:
+        distance = random.randrange(0, 9)
+        path = car.move(distance)
+        if path:
+            race_dist += car.trip_distance
+            print(f"{car.color} {car.model} проїхав {distance} км. Загальна відстань: {race_dist} км.")
+            if race_dist >= desired_dist:
+                print(f"\n{car.color} {car.model} переміг!")
+                break
+            fuel_empty = False
 
-    # Оновлення загальної відстані гонки
-    race_dist += max(dist1, dist2, dist3)
-
-    # Перевірка переможця
-    if any(car.trip_distance >= desired_dist for car in [car1, car2, car3]):
-        winner = next(car for car in [car1, car2, car3] if car.trip_distance >= desired_dist)
-        print(f"Автомобіль {winner.color} {winner.model} переміг! Проїхавши {winner.trip_distance} км.")
+    if fuel_empty:
+        print("Усі машини залишилися без пального. Гонка завершена.")
         break
 
-# Виведення результатів гонки
-for car in [car1, car2, car3]:
-    print(f"Автомобіль {car.color} {car.model} проїхав {car.trip_distance} км. Запас палива: {car.fuel} л.")
+print(f"{car1.model} проїхав {car1.trip_distance} км, залишилося {car1.fuel} палива.")
+print(f"{car2.model} проїхав {car2.trip_distance} км, залишилося {car2.fuel} палива.")
+print(f"{car3.model} проїхав {car3.trip_distance} км, залишилося {car3.fuel} палива.")
